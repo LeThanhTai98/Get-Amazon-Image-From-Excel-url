2fb967d44b21a26a4bffd44daeacc09d9593b0e1
Thanh Tai <Thanh Tai@DESKTOP-6H9QI3M>
2020-04-17 17:17:54 +0700

    init project

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const readXlsxFile = require('read-excel-file/node');
+
+var i = 0 ;
+var arrayLink ;
+// đường dẫn tới file excel
+readXlsxFile('E:/1111.xlsx').then((rows) => {
+rows.forEach(row => {
+   
+    const Scraper = require("@jonstuebe/scraper");
+
+    // run inside of an async function
+    (async () => {
+      const data = await Scraper.scrapeAndDetect(row[1]
+      );
+    
+      
+      const download = require('image-downloader')
+     
+      // lọc bỏ các kí tự đặc biệt 
+      var res = data[itemprop="title"].replace(/(\|)|(\')/i, " ");
+      const options = {
+        url: data[itemprop="image"],
+        //đường dẫn đến vị trí lưu file                     //tên file//đuôi file
+        dest: 'C:/Users/Thanh Tai/Desktop/webPublic/TestVScode/'+res+".jpg"                
+      }
+       
+      download.image(options)
+        .then(({ filename , name = data[itemprop="title"] }) => {
+          console.log('Saved to', filename)  
+        })
+        .catch((err) => console.error(err))
+     
+    })();
+});
+})
---
+++ regex.js
@@ -0,0 +1,6 @@
+
+(async()=>{
+    var name = "10 Years Old Birthday Shirt | OMG I'm Ten Shirt";
+    var res = name.replace(/(\|)|(\')/, " ");
+    console.log(res);
+})
